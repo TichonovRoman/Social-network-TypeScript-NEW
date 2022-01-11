e5c945f5b9ec449ce42f4b3180db8306978d03f8
TichonovRoman <romikt@inbox.ru>
2022-01-11 18:08:59 +0300

    21: dialogs Page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,30 @@ import s from "./Dialogs.module.css"
 
 const Dialogs: React.FC = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + " " + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi!</div>
+                <div className={s.message}>How are you doing?</div>
+                <div className={s.message}>What are you doing?</div>
+            </div>
         </div>
     );
 };
