d1fe38764e0c281e9b7ce17b02344be957faad3a
TichonovRoman <romikt@inbox.ru>
2022-02-16 10:04:16 +0300

    add profile-reducer.test

---
+++ src/redux/profile-reducer.test.tsx
@@ -0,0 +1,41 @@
+import {ProfilePageType} from "./store";
+import profileReducer, {addPostActionCreator, updateNewPostTextActionCreator} from "./profile-reducer";
+
+
+
+test("in dialogs should add post", () => {
+
+    let initialState: ProfilePageType = {
+        posts: [
+            {id: 1, message: "Hi, how are yuo?", likesCount: 15},
+            {id: 2, message: "It`s my first post", likesCount: 20},
+            {id: 3, message: "Hi", likesCount: 56},
+            {id: 4, message: "Cool", likesCount: 20},
+        ],
+        newPostText: 'it-kamasutra.com'
+    }
+
+    initialState = profileReducer(initialState, addPostActionCreator())
+
+    expect( initialState.posts[4].message).toBe('it-kamasutra.com')
+
+})
+
+test("in dialogs should update post", () => {
+
+    let initialState: ProfilePageType = {
+        posts: [
+            {id: 1, message: "Hi, how are yuo?", likesCount: 15},
+            {id: 2, message: "It`s my first post", likesCount: 20},
+            {id: 3, message: "Hi", likesCount: 56},
+            {id: 4, message: "Cool", likesCount: 20},
+        ],
+        newPostText: 'it-kamasutra.com'
+    }
+
+    initialState = profileReducer(initialState, updateNewPostTextActionCreator("Hi peoples"))
+
+    expect( initialState.newPostText).toBe("Hi peoples")
+
+})
+
--- src/redux/profile-reducer.tsx
+++ src/redux/profile-reducer.tsx
@@ -1,7 +1,7 @@
 import {ActionsTypes, MessagesDataType, PostsDataType, ProfilePageType} from "./store";
 
-const ADD_POST = `ADD-POST`
-const UPDATE_NEW_POST_TEXT = `UPDATE-NEW-POST-TEXT`
+export const ADD_POST = `ADD-POST`
+export const UPDATE_NEW_POST_TEXT = `UPDATE-NEW-POST-TEXT`
 
 
 let initialState: ProfilePageType = {
