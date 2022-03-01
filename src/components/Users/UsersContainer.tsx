63215271504c5b0ba7fc2ebb1d2b35afff6b52ed
TichonovRoman <romikt@inbox.ru>
2022-03-01 15:57:33 +0300

    53: class Components

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import Users from "./Users";
+
 import {connect} from "react-redux";
 import {AppStateType} from "../../redux/redux-store";
 import {Dispatch} from "redux";
 import {followAC, setUsersAC, unfollowAC, UsersDataType} from "../../redux/users-reducer";
+import Users from "./UsersС";
 
 type mapStateToPropsType = {
     users: Array<UsersDataType>
