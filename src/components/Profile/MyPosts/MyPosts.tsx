583f612b6ed22a74d6b87b20f8aafe18aeecbbbf
TichonovRoman <romikt@inbox.ru>
2022-04-28 21:07:06 +0300

    87: React.Memo,

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,32 +2,25 @@ import React from 'react';
 import s from "./MyPosts.module.css"
 import Post from "./Post/Post";
 import {PostsPropsType} from "./MyPostsContainer";
-import MessageForm, {AddMessageForm} from "../../universalTextarea/MessageForm";
+import {AddMessageForm} from "../../universalTextarea/MessageForm";
 
 
-const MyPosts = (props: PostsPropsType) => {
+const MyPosts = React.memo((props: PostsPropsType) => {
 
     let postsElements = props.posts.map(p => <Post key={p.id} message={p.message} likesCount={p.likesCount}/>)
 
-
-
-return (
-    <div className={s.postsBlock}>
-        <h3>My posts</h3>
-        <div>
-
-            <AddMessageForm onSubmit={props.addPost}
-                         placeholderText={"Enter your post"}
-                         textMaxLength={100}
-
-            />
-
+    return (
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
+            <div>
+                <AddMessageForm onSubmit={props.addPost}
+                                placeholderText={"Enter your post"}
+                                textMaxLength={100}
+                />
+            </div>
+            <div className={s.posts}>{postsElements}</div>
         </div>
-        <div className={s.posts}>{postsElements}</div>
-    </div>
-)
-
-}
-
+    )
+});
 
 export default MyPosts;
