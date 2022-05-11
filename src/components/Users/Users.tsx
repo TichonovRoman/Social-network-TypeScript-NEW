97c2bc5f77112863774a27a44d04b4d792bf6fe6
TichonovRoman <romikt@inbox.ru>
2022-05-11 15:13:40 +0300

    93: pagination

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,6 +1,6 @@
 import styles from "./users.module.css";
 import NotFoundFoto from "../../img/FotoNotFound.jpg";
-import React from "react";
+import React, {useState} from "react";
 import {follow, toogleFollowingProgress, unfollow, UsersDataType} from "../../redux/users-reducer";
 import {NavLink} from "react-router-dom";
 import axios from "axios";
@@ -22,21 +22,61 @@ type UsersPropsType = {
 
 const Users = (props: UsersPropsType) => {
 
+
+
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
     let pages = []
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
-        if (i == 20) break;//Слишком много страниц, поэтому ограничиваем в 20 шт
-    }
+        }
+    const portionSize = 5 // размер количества страниц для одновременного отображения
+
+    let portionCount = Math.ceil(pagesCount / portionSize);
+    let [portionNumber, setPortionNumber] = useState<number>(1)
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize;
+
+
+
+
     return <div>
         <div style={{paddingBottom: "5px"}}>
-            {pages.map((c, index) => {
-                return <button key={index}
-                               onClick={(e) => {
-                                   props.onPageChanged(c)
-                               }}
-                               className={props.currentPage === c ? styles.selectedPage : ""}>{c}</button>
-            })}
+
+            {portionNumber > 1 && <button style={{backgroundColor: "bisque"}} onClick={() => {
+                setPortionNumber(portionNumber - 1)
+            }}>Prev</button>}
+
+            {pages
+                .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                .map((c, index) => {
+                    return <button style={{width: "40px"}} key={index}
+                                   onClick={(e) => {
+                                       props.onPageChanged(c)
+                                   }}
+                                   className={props.currentPage === c ? styles.selectedPage : ""}>{c}</button>
+                })
+            }
+            {portionCount > portionNumber && <button style={{backgroundColor: "bisque"}} onClick={() => {
+                setPortionNumber(portionNumber + 1)
+            }}>Next</button>}
+
+
+
+
+
+            {/*{pages.map((c, index) => {*/}
+            {/*    return <button key={index}*/}
+            {/*                   onClick={(e) => {*/}
+            {/*                       props.onPageChanged(c)*/}
+            {/*                   }}*/}
+            {/*                   className={props.currentPage === c ? styles.selectedPage : ""}>{c}</button>*/}
+            {/*})}*/}
+
+
+
+
+
+
         </div>
 
 
